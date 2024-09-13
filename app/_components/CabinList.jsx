814a3f686a235f2b35cb106d6c12b3fb391b7700
AlaaHamdy <alaahamdy2197@gmail.com>
2024-09-13 14:41:19 +0300

    feat(cabin list): wrapped cabin list into react suspense

---
+++ app/_components/CabinList.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import CabinCard from './CabinCard';
+import { getCabins } from "../_lib/data-service";
+const CbinList = async () => {
+  const cabins = await getCabins();
+  if (!cabins.length) return null;
+  return (
+    <>
+      {cabins.length > 0 && (
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+          {cabins.map((cabin) => (
+            <CabinCard cabin={cabin} key={cabin.id} />
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CbinList
